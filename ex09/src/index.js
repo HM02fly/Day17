e8bbc301b59e170fec6e1b9bbc76026f67c09226
HM02fly <46242213+HM02fly@users.noreply.github.com>
2021-03-25 10:59:13 +0100

    Add files via upload

---
+++ ex09/src/index.js
@@ -0,0 +1,66 @@
+// Lodash
+const _ = require('lodash');
+
+// Only change code below this line
+// users nested array with four objects starts here
+const users = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 24,
+    gender: 'male',
+  },
+  {
+    id: 2,
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 5,
+    gender: 'female',
+  },
+  {
+    id: 3,
+    firstName: 'Jim',
+    lastName: 'Carrey',
+    age: 54,
+    gender: 'male',
+  },
+  {
+    id: 4,
+    firstName: 'Kate',
+    lastName: 'Winslet',
+    age: 40,
+    gender: 'female',
+  },
+];
+// users nested array with four objects ends here
+const getUsers = () => {
+  // getUsers function - list of users starts here
+  var output = '';
+  for (let i = 0; i < users.length; i++) {
+    output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
+  }
+  console.log(output);
+  return output;
+};
+// getUsers function - list of users ends here
+// findUserById(id) function starts here
+function findUserById(id) {
+  try {
+    // add appropriate code here
+    var user = _.filter(users, { id: id });
+    var iFoundUser = `${user[0].id} - ${user[0].firstName} ${user[0].lastName} is ${user[0].age}, ${user[0].gender}`;
+    console.log(iFoundUser);
+    return iFoundUser;
+  } catch (error) {
+    console.log("Cannot read property 'id'"); // Change this line
+    return "Cannot read property 'id'"; // Change this line
+  }
+}
+// findUserById(id) function ends here
+// Only change code above this line
+getUsers();
+findUserById(3); // Change this line
+findUserById(5); // Change this line
+
+module.exports = findUserById;
